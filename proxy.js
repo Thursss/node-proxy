19cdf6fc1fd2044b4ff5fc99f2a152ba702f58c0
zhousiping <1720557879@qq.com>
2022-06-14 10:12:10 +0800

    feat: 携带请求头

--- proxy.js
+++ proxy.js
@@ -25,24 +25,20 @@ app.use(
     pathRewrite: {
       "^/api": "",
     },
-    onProxyReq(proxyReq, req, res) {
-      console.log(req);
-      proxyReq.setHeader("cookie", "token=AA23D3CE66491443806C06D2BE49D892");
+    onProxyReq(proxyReq, req) {
+      // 将本地请求的头信息复制一遍给代理。
+      // 包含cookie信息，这样就能用登录后的cookie请求相关资源
+      Object.keys(req.headers).forEach(function (key) {
+        proxyReq.setHeader(key, req.headers[key]);
+      });
     },
-    // onProxyReq(proxyReq, req) {
-    //   // 将本地请求的头信息复制一遍给代理。
-    //   // 包含cookie信息，这样就能用登录后的cookie请求相关资源
-    //   Object.keys(req.headers).forEach(function (key) {
-    //     proxyReq.setHeader(key, req.headers[key]);
-    //   });
-    // },
-    // onProxyRes(proxyRes, req, res) {
-    //   // 将服务器返回的头信息，复制一遍给本地请求的响应。
-    //   // 这样就能实现 执行完登录后，本地的返回请求中也有相关cookie，从而实现登录功能代理。
-    //   Object.keys(proxyRes.headers).forEach(function (key) {
-    //     res.append(key, proxyRes.headers[key]);
-    //   });
-    // },
+    onProxyRes(proxyRes, req, res) {
+      // 将服务器返回的头信息，复制一遍给本地请求的响应。
+      // 这样就能实现 执行完登录后，本地的返回请求中也有相关cookie，从而实现登录功能代理。
+      Object.keys(proxyRes.headers).forEach(function (key) {
+        res.append(key, proxyRes.headers[key]);
+      });
+    },
   })
 );
 
